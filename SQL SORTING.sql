8db6c79fb0a06ef9046ae14e25a7e83ddf18c0a9
Zibaloluwa <hephzibanoiki@gmail.com>
2025-10-19 20:16:22 +0100

    Updated SQL filtering and sorting

--- SQL SORTING.sql
+++ SQL SORTING.sql
@@ -1,10 +1,21 @@
-SELECT * FROM public.hephziba_customer_data
+-- SELECT * FROM public.hephziba_customer_data
 -- ORDER BY customer_id DESC 
---SELECT first_name, email, gender, d_o_b FROM public.hephziba_customer_data
-SELECT membership, first_name, customer_id, age, ip_address FROM public.hephziba_customer_data
-ORDER BY age, customer_id ASC
-SELECT gender, age, first_name, last_name FROM public.hephziba_customer_data
-SELECT DISTINCT (gender) FROM public.hephziba_customer_data
-SELECT DISTINCT gender FROM public.hephziba_customer_data
-ORDER BY gender ASC
-SELECT COUNT (DISTINCT gender) FROM public.hephziba_customer_data
+-- SELECT first_name, email, gender, d_o_b FROM public.hephziba_customer_data
+-- SELECT membership, first_name, customer_id, age, ip_address FROM public.hephziba_customer_data
+-- ORDER BY age, customer_id ASC
+-- SELECT gender, age, first_name, last_name FROM public.hephziba_customer_data
+-- SELECT DISTINCT (gender) FROM public.hephziba_customer_data
+-- SELECT DISTINCT gender FROM public.hephziba_customer_data
+-- ORDER BY gender ASC
+-- SELECT COUNT (DISTINCT gender) FROM public.hephziba_customer_data
+-- SELECT gender FROM public.hephziba_customer_data
+-- WHERE gender = 'Female' 
+--SELECT * FROM public.hephziba_customer_data
+--WHERE age  BETWEEN 1 AND 200
+--SELECT DISTINCT gender FROM public.hephziba_customer_data WHERE age  BETWEEN 1 AND 200
+--SELECT first_name, last_name FROM public.hephziba_customer_data WHERE age  BETWEEN 1 AND 200
+--SELECT * FROM public.hephziba_customer_data WHERE first_name LIKE '%c'
+--SELECT * FROM public.hephziba_customer_data WHERE email LIKE '%princeton.edu'
+--SELECT last_name, gender, preferences, d_o_b, age FROM public.hephziba_customer_data 
+--WHERE age IN (18, 20)
+--SELECT first_name, d_o_b, gender, age FROM public.hephziba_customer_data WHERE age <= 15
